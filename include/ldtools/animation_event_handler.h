76077e4c113feb0b1c6dc06e809293cbb3dd0f97
The Marlboro Man <marlborometal@gmail.com>
2025-04-13 18:29:45 +0200

    Some documentation

--- include/ldtools/animation_event_handler.h
+++ include/ldtools/animation_event_handler.h
@@ -4,13 +4,24 @@
 
 namespace ldtools {
 
+/**
+*Monitors an animation for the ocurrence of a specific frame. The idea here
+*is to instantiate as many of these as needed to monitor the animations of
+*specific entities. The use case looks like:
+* - instantiate this with the callback, a function that will get the animation
+*   and frame indexes.
+* - when a frame is chosen for an entity call "update" with the animation and frame.
+* - the callback will fire each time the frame / animation changes.
+*/
 class animation_event_handler {
 
 	public:
 
 	using                       cb=std::function<void (std::size_t, std::size_t)>;
 
+	//! Sets the callback.
 	void                        set(cb);
+	//! Must be called each time the animation or frame changes.
 	void                        update(std::size_t, std::size_t);
 
 	private:
